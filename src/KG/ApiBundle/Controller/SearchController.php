d98fdaa5f7f5ebc120163374ef8650a032750036
KristenGarnier <garnier.kristen@icloud.com>
2016-02-11 12:04:19 +0100

    Adding a order by

--- src/KG/ApiBundle/Controller/SearchController.php
+++ src/KG/ApiBundle/Controller/SearchController.php
@@ -4,21 +4,41 @@ namespace KG\ApiBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Util\Codes;
 
 class SearchController extends FOSRestController
 {
     public function getSearchAction(Request $request)
     {
-        try{
-            return $this->get('get_or_404')->checkArray(
-                $this->getDoctrine()->getRepository('KGUserBundle:User')->findBy($request->query->all())
+        try {
+            $data = $this->get('verify_data')->checkNoLenght(
+                $request->query->all(),
+                $this->get('white_list')->user()
             );
-        } catch (\Exception $e){
+
+            $orderBy= [];
+
+            if($request->get('orderBy') && $request->get('order')){
+                $orderBy = [$request->get('orderBy') => $request->get('order')];
+            }
+
+
+            $queryRes =  $this->get('get_or_404')->checkArray(
+                $this->getDoctrine()->getRepository('KGUserBundle:User')->findBy($data, $orderBy)
+            );
+
             $routeOptions = array(
                 '_format' => $request->get('_format')
             );
 
-            if($e->getCode()){
+            return $this->view($queryRes, Codes::HTTP_OK, $routeOptions);
+
+        } catch (\Exception $e) {
+            $routeOptions = array(
+                '_format' => $request->get('_format')
+            );
+
+            if ($e->getCode()) {
                 return $this->view(['error' => $e->getMessage()], $e->getCode(), $routeOptions);
             }
         }
